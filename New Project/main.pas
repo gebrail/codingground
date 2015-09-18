1b163aebaa27ef731a177058503d4984e8e81e00
Wilson Gebrail Barrera Hernandez <wilsongebrail@hotmail.com>
2015-09-18 10:43:02 -0500

    Created by Coding Ground Client

---
+++ New Project/main.pas
@@ -0,0 +1,12 @@
+uses crt;
+var n1,n2,n3,n4,n5,media:real;
+begin
+clrScr;
+write('ingrese el primer valor '); readln(n1);
+write('ingrese el segundo valor valor '); readln(n2);
+write('ingrese el tercer valor '); readln(n3);
+write('ingrese el cuarto valor valor '); readln(n4);
+write('ingrese el quinto valor '); readln(n5);
+media:=(n1+n2+n3+n4+n5)/2;
+writeln('la media es ', media);
+end.
